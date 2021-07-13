86b4b566f3e4019e17b427ed9f3639524b29a374
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2021-07-13 10:56:42 -0700

    July 13 AM changes

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
 <footer class="blog-footer">
-	<p><a href="#">Back to top</a></p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<?php dynamic_sidebar( 'footer-one' ); ?>
+			</div>
+			<div class="col-md-4">
+				<p><a href="#">Back to top</a></p>
+			</div>
+			<div class="col-md-4">
+				<?php dynamic_sidebar( 'footer-two' ); ?>
+			</div>
+		</div>
+	</div>
 	<?php wp_footer(); ?>
 </footer>
--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
   <div class="col-md-4">
     <div class="position-sticky" style="top: 2rem;">
-     <?php get_template_part('sidebar'); ?>
+     <?php get_sidebar(); ?>
   </div>
 </div>
 
